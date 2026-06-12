29b83f9ce7ce04f53a3de7a047077ea7b083a377
Валерия Футкарадзе <jezzpol@mail.ru>
2026-06-12 23:40:01 +0300

    first init commit

---
+++ src/test/java/com/Futkaradze/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Futkaradze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
