ebf6399ab3abb09725b8fea74596e1e1d6c64aa1
Валерия Футкарадзе <jezzpol@mail.ru>
2026-06-13 14:23:00 +0300

    Добавлены сущности User, Post, Comment

---
+++ src/main/java/com/Futkaradze/models/Comment.java
@@ -0,0 +1,33 @@
+package com.Futkaradze.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime craetedAt;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToMany
+    private List<User> liked = new ArrayList<>();
+
+}
